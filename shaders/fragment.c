ac36f6f64225ffaa5d515058382bed454ad8c1c3
JoeCosmic <111359232+JoeCosmic@users.noreply.github.com>
2022-08-16 15:57:35 +0300

    Delete fragment.c

--- shaders/fragment.c
+++
@@ -1,7 +0,0 @@
-@binding(1) @group(0) var Sampler : sampler;
-      @binding(2) @group(0) var textureData : texture_2d<f32>;
-      @stage(fragment)
-      fn main(@location(0) vUV: vec2<f32>) -> @location(0) vec4<f32> {
-      let textureColor:vec3<f32> = (textureSample(textureData, Sampler, vUV)).rgb;
-      return vec4<f32>(textureColor, 1.0);
-    }
